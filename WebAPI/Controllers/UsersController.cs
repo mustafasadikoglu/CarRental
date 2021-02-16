1562f396561530edad141a5bc003d137a0bcec86
mustafasadikoglu <66087800+mustafasadikoglu@users.noreply.github.com>
2021-02-16 15:49:05 +0300

    v1.8

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -64,7 +64,7 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }
 
-        [HttpGet("getbrand")]
+        [HttpGet("getuser")]
         public IActionResult Get(int id)
         {
             var result = _userService.Get(id);
